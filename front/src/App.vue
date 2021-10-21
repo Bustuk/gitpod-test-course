109552698386142f4409c4254b1f3881d2a9efaf
Łukasz Wołodkiewicz <wolodkiewiczlukasz@gmail.com>
2021-10-21 20:57:52 +0000

    test socketów i http

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,16 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <p>Socket Value: {{ socketValue }}</p>
+    <p>Fetch Value: {{ fetchedValue }}</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import io from 'socket.io-client';
+import { ref } from 'vue';
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  setup() {
+    const socketValue = ref(0)
+    const fetchedValue = ref(0)
+    const socket = io('https://8069-violet-shrew-p49fz8pd.ws-eu18.gitpod.io', {
+      reconnection: false,
+      transports: ["websocket", "polling"]
+    });
+
+    socket.on("status", status => {
+        socketValue.value = status;
+    });
+    fetch('https://8069-violet-shrew-p49fz8pd.ws-eu18.gitpod.io')
+      .then(response =>{console.log(response); return response.json()})
+      .then(data => fetchedValue.value = data.test);
+    return {
+     fetchedValue,
+     socketValue
+    }
+  },
 }
 </script>
 
--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-const dialer = require("dialer");
+const express = require("express");
+const { Server } = require("socket.io");
+const app = express();
 
-console.log(dialer)
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
+  next();
+});
+
+const server = app.listen(8069, function() {
+  console.log("Sitecall app listening on port 8069!");
+});
+
+const io = new Server(server)
+
+io.on("connection", (socket) => {
+  console.log('Połączono socket');
+  io.emit("status", 5555);
+});
+  
+app.get("/", (req,res) => {
+  console.log('Request!');
+  res.json({'test': 5})
+});
